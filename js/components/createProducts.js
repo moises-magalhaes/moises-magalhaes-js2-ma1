c996f7fcc52d7fd6cbdfde97e99fe37af720f588
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-03-25 12:13:39 +0100

    filter and message - empty container

--- js/components/createProducts.js
+++ js/components/createProducts.js
@@ -1,11 +1,16 @@
+import displayMessage from "./displayMessage.js";
+
 export default function createProducts(products) {
     const container = document.querySelector(".product-container");
 
     container.innerHTML = "";
 
+    if(products.length === 0) {
+        displayMessage("warning", "No products found", ".product-container");
+    }
+
     products.forEach(function(product){
 
-        //instead of -> container.innerHTML he uses
         container.innerHTML += `<div class="product">
                     <h4>${product.name}</h4>
                     <p>Price:${product.price}</p>
